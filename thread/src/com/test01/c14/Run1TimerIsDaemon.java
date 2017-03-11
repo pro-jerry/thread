e35effdd76eb1752e2628815daaa88c0025a0f80
Administrator <Administrator@DESKTOP-427B1LM>
2017-03-11 22:32:58 +0800

    1

---
+++ thread/src/com/test01/c14/Run1TimerIsDaemon.java
@@ -0,0 +1,28 @@
+package com.test01.c14;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Run1TimerIsDaemon {
+
+	private static Timer timer = new Timer(true);
+	public static class MyTask extends TimerTask{
+		@Override
+		public void run() {
+			System.out.println("运行了,时间为:"+new Date());
+		}
+		
+	}
+	public static void main(String[] args) throws ParseException {
+		
+		MyTask task  = new MyTask();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String dataString="2017-03-12 12:20:15";
+		Date dateRef = sdf.parse(dataString);
+		System.out.println("字符串时间:"+dateRef.toLocaleString()+"当前时间:"+new Date().toLocaleString());
+		timer.schedule(task, dateRef);
+	}
+}
---
+++ thread/src/com/test01/c14/Test.java
@@ -0,0 +1,9 @@
+package com.test01.c14;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Integer i=null;
+		System.out.println(i==1);
+	}
+}
